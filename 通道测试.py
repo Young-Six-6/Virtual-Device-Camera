1a638c0ab3ced14f960a628a5fc04722e524f33d
Young-Six-6 <3478525117@qq.com>
2025-10-13 22:42:45 +0800

    更新注释
    说明文件和注释，重命名原项目的“debug”目录做区分

--- 通道测试.py
+++ 通道测试.py
@@ -4,10 +4,10 @@ import struct
 # 连接到命名管道
 pipe = open(r'\\.\pipe\vcam_pipe', 'wb')
 
-# 发送 640x480 的 RGB 数据（示例：红色画面）
+# 发送 1920x1080 的 RGB 数据
 width, height = 1920, 1080
 pipe.write(struct.pack('<ii', width, height))  # 发送宽高
-red_data = b'\x00\x00\x00' * (width * height)  # RGB 红色（每个像素 R=255, G=0, B=0）
+red_data = b'\x00\x00\x00' * (width * height)  # 画面
 while True:
     pipe.write(red_data)
     pipe.flush()
